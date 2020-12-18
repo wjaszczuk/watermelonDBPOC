d37b9be5c13b7fccbe3a7290b56bee4d10e4033c
Wiktor Jaszczuk <wjaszczuk@spyro-soft.com>
2020-12-18 14:24:34 +0100

    DB sync with a fallback mechanism (HTTP)

--- src/models/dbSync.js
+++ src/models/dbSync.js
@@ -6,10 +6,32 @@ export function dbSync(database) {
     return synchronize({
       database,
       pullChanges: async ({lastPulledAt, schemaVersion, migration}) => {
+        try {
+          const response = await fetch(`https://my.backend/sync`, {
+            body: JSON.stringify({lastPulledAt, schemaVersion, migration}),
+          });
+          if (!response.ok) {
+            throw new Error(await response.text());
+          }
+          const {changes, timestamp} = await response.json();
+          return {changes, timestamp};
+        } catch (e) {}
+
         return {changes: getMockData(), timestamp: new Date().valueOf()};
       },
       pushChanges: async ({changes, lastPulledAt}) => {
-        console.log('Push changes', changes, lastPulledAt);
+        try {
+          const response = await fetch(
+            `https://my.backend/sync?last_pulled_at=${lastPulledAt}`,
+            {
+              method: 'POST',
+              body: JSON.stringify(changes),
+            },
+          );
+          if (!response.ok) {
+            throw new Error(await response.text());
+          }
+        } catch (e) {}
       },
       migrationsEnabledAtVersion: 1,
     });
